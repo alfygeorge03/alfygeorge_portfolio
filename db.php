60eb59763a4d102794f00d5e7c588dca02a8008d
Alfy_George <alfy@webqua.com>
2026-05-25 13:18:05 +0530

    change db and chats

--- db.php
+++ db.php
@@ -12,14 +12,12 @@ $pass = getenv('MYSQLPASSWORD');
 try {
 
     $conn = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname",
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
         $user,
         $pass
     );
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    echo "Database Connected";
 
 } catch(PDOException $e) {
 
